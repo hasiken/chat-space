e8cb7366eb4fa121c23d791eec14b8966bad8ed8
hasiken <kentarou.123.99@gmail.com>
2020-02-27 15:32:46 +0900

    よろしくお願いします

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,42 +3,41 @@
     if ( message.image ) {
      var html= 
      `<div class="message" >
-   
-     <div class="upper-message">
-     <div class="upper-message__user-name">
-     ${message.user_name}
-     </div>
-     <div class="upper-message__date">
-     ${message.created_at}
-     </div>
+       <div class="upper-message">
+        <div class="upper-message__user-name">
+          ${message.user_name}
+        </div>
+        <div class="upper-message__date">
+          ${message.created_at}
+       </div>
      </div>
      <div class="lower-message">
-     <p class="lower-message__content">
-     ${message.content}
-     </p>
-     </div>
-     <img src=${message.image} >
-     </div>`
-     return html;
-   } else {
-     var html =
-     `<div class="message" >
-       <div class="upper-message">
-         <div class="upper-message__user-name">
-           ${message.user_name}
-         </div>
-         <div class="upper-message__date">
-           ${message.created_at}
-         </div>
-       </div>
-       <div class="lower-message">
-         <p class="lower-message__content">
-           ${message.content}
-         </p>
+       <p class="lower-message__content">
+         ${message.content}
+       </p>
+    </div>
+    <img src=${message.image} >
+  </div>`
+  return html;
+} else {
+  var html =
+   `<div class="message" >
+     <div class="upper-message">
+      <div class="upper-message__user-name">
+        ${message.user_name}
+      </div>
+      <div class="upper-message__date">
+       ${message.created_at}
+      </div>
+    </div>
+    <div class="lower-message">
+      <p class="lower-message__content">
+       ${message.content}
+      </p>
        </div>
      </div>`
    return html;
- };
+  };
  }
 $('#new_message').on('submit', function(e){
      e.preventDefault();
