94296e68b1ed9b705660a5e095da12c3df10dc6f
hasiken <kentarou.123.99@gmail.com>
2020-03-01 11:58:23 +0900

    onegai

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+
+server '18.177.41.27', user: 'ec2-user', roles: %w{app db web}
